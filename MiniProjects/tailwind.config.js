c7a9bd53371760250637d097bf7daef007a8fe47
Mehul Padhiyar <mehulpadhiyar597@gmail.com>
2024-12-20 17:52:28 +0530

    Added pricing cards mini project

--- MiniProjects/tailwind.config.js
+++ MiniProjects/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./Email_Subscribe/*.html'],
+  content: ['./Email_Subscribe/*.html', './Pricing_Card/*.html'],
   theme: {
     extend: {},
   },
