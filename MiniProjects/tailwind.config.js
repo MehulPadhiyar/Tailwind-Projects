1759b4b6f5b005b3da8b0cf31685fe982c22e0f0
Mehul Padhiyar <mehulpadhiyar597@gmail.com>
2024-12-22 10:32:51 +0530

    Added product modal project

--- MiniProjects/tailwind.config.js
+++ MiniProjects/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./Email_Subscribe/*.html', './Pricing_Card/*.html'],
+  content: ['./Email_Subscribe/*.html', './Pricing_Card/*.html', './Product_Modal/*.html'],
   theme: {
     extend: {},
   },
