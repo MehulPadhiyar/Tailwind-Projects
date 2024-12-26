3986beb983761565e65dafe10f79e45f703cd7ab
Mehul Padhiyar <mehulpadhiyar597@gmail.com>
2024-12-26 15:01:31 +0530

    Added Login Modal project

--- MiniProjects/tailwind.config.js
+++ MiniProjects/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./Email_Subscribe/*.html', './Pricing_Card/*.html', './Product_Modal/*.html'],
+  content: ['./Email_Subscribe/*.html', './Pricing_Card/*.html', './Product_Modal/*.html', './Login_Modal/*.html'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Mulish', 'sans-serif'],
+      mono: ['Rokkitt', 'monospace'],
+    },
   },
   plugins: [],
 };
