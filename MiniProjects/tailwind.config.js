c69b150c75e7c4bc2ba47ddf1342b5f5ff84f6bc
Mehul Padhiyar <mehulpadhiyar597@gmail.com>
2024-12-29 14:49:11 +0530

    Added image gallery project

--- MiniProjects/tailwind.config.js
+++ MiniProjects/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./Email_Subscribe/*.html', './Pricing_Card/*.html', './Product_Modal/*.html', './Login_Modal/*.html'],
+  content: [
+    './Email_Subscribe/*.html',
+    './Pricing_Card/*.html',
+    './Product_Modal/*.html',
+    './Login_Modal/*.html',
+    './Image_Gallery/*.html',
+  ],
   theme: {
     extend: {},
     fontFamily: {
