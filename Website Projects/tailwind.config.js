d5d80c2dbb1d8ffa6c19eccc9815755269ed4300
Mehul Padhiyar <mehulpadhiyar597@gmail.com>
2025-01-08 18:14:19 +0530

    Added clipboard website

---
+++ Website Projects/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./clipboard/*.html'],
+  theme: {
+    colors: {
+      strongCyan: 'hsl(171, 66%, 44%)',
+      lightBlue: 'hsl(233, 100%, 69%)',
+      darkGrayishBlue: 'hsl(210, 10%, 33%)',
+      grayishBlue: 'hsl(201, 11%, 66%)',
+    },
+    fontFamily: {
+      sans: ['Bai Jamjuree', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
